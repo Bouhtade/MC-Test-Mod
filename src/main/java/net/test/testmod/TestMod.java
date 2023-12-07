f4bed03dcf7f7f103e1a01c75dc159ca4c90fcae
Bouhtade <noe.bassin@divtec.ch>
2023-12-07 12:09:11 +0100

    Made new creative tab

--- src/main/java/net/test/testmod/TestMod.java
+++ src/main/java/net/test/testmod/TestMod.java
@@ -13,6 +13,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.test.testmod.item.ModCreativeModTabs;
 import net.test.testmod.item.ModItems;
 import org.slf4j.Logger;
 
@@ -26,6 +27,8 @@ public class TestMod
     public TestMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModCreativeModTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
 
